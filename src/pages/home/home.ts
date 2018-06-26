c03dbf5f5dce0017fcb7f9de71f3840aeb1a8eac
Matoro Andani <andanimatoro@gmail.com>
2018-06-26 14:01:31 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,36 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  value1: number;
+  value2: number;
   constructor(public navCtrl: NavController) {
-
+      this.value1=0;
+      this.value2=0;
   }
 
+  
+  teamAScore(num : number){
+    if(num == 3){
+      this.value1 += num;
+    }else if( num == 2){
+      this.value1 += num;
+    }else{
+      this.value1 += num;
+    }
+  }
+
+  teamBScore(num : number){
+    if(num == 3){
+      this.value2 += num;
+    }else if( num == 2){
+      this.value2 += num;
+    }else{
+      this.value2 += num;
+    }
+  }
+
+  submit(){
+    this.value1 = 0;
+    this.value2 = 0;
+  }
 }
